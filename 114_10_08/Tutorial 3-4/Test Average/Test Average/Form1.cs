fadaf6388f2b99427203df8219b0b6cf5b4e41bb
leeyihsuan3 <a114221054@mail.shu.edu.tw>
2025-10-08 12:59:44 +0800

    114_10_08

---
+++ 114_10_08/Tutorial 3-4/Test Average/Test Average/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 計算平均分數
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double score1 = double.Parse(test1TextBox.Text);
+                double score2 = double.Parse(test2TextBox.Text);
+                double score3 = double.Parse(test3TextBox.Text);
+
+                double average = (score1 + score2 + score3) / 3;
+                averageLabel.Text = average.ToString("F2");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("請輸入有效的分數。", "輸入錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 清除所有欄位
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+
+            double test1, test2, test3;
+            try
+            {
+
+
+
+                test1 = double.Parse(test1TextBox.Text);
+                test2 = double.Parse(test2TextBox.Text);
+                test3 = double.Parse(test3TextBox.Text);
+
+                double average = (test1 + test2 + test3) / 3.0;
+                averageLabel.Text = average.ToString("n2");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("例外發生:", ex.Message);
+                test1TextBox.Text = "";
+                test2TextBox.Text = "";
+                test3TextBox.Text = "";
+                averageLabel.Text = "";
+                test1TextBox.Focus();
+
+
+            }
+        }
+        // 關閉表單
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ 114_10_08/Tutorial 3-5/Change Counter/Change Counter/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Change_Counter
+{
+    public partial class Form1 : Form
+    {
+        const decimal FIVE_CENTS = 5.0m;
+        const decimal TEN_CENTS = 10.0m;    
+        const decimal TWENTY_FIVE_CENTS =25.0m;    
+        const decimal FIFTY_CENTS =50.0m;  
+
+
+        private decimal total;
+        // 硬幣數量
+        private int fiveCentsCount = 0;
+        private int tenCentsCount = 0;
+        private int twentyFiveCentsCount = 0;
+        private int fiftyCentsCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 計算總金額
+        private decimal CalculateTotal()
+        {
+            return fiveCentsCount * 0.05m +
+                   tenCentsCount * 0.10m +
+                   twentyFiveCentsCount * 0.25m +
+                   fiftyCentsCount * 0.50m;
+        }
+
+        // 更新顯示總金額
+        private void UpdateTotalDisplay()
+        {
+            totalLabel.Text = $"{CalculateTotal():C2}";
+        }
+
+        // 事件：點擊 5c 硬幣
+        private void fiveCentsPictureBox_Click(object sender, EventArgs e)
+        {
+           
+            total = total +=5; 
+            totalLabel.Text= total.ToString();
+        }
+
+        // 事件：點擊 10c 硬幣
+        private void tenCentsPictureBox_Click(object sender, EventArgs e)
+        {
+          
+            total = total + =10;
+            totalLabel.Text = total.ToString();
+
+           
+        }
+
+        // 事件：點擊 25c 硬幣
+        private void twentyFiveCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            
+            total = total + =25;
+            totalLabel.Text = total.ToString();
+            
+        }
+
+        // 事件：點擊 50c 硬幣
+        private void fiftyCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total = total + =50;
+            totalLabel.Text = total.ToString();
+            
+        }
+
+        // 事件：離開按鈕
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
