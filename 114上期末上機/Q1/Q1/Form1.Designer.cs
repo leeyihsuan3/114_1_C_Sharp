23b3a1f01799d5db84ce9a0121142383d118d72b
leeyihsuan3 <a114221054@mail.shu.edu.tw>
2025-12-24 11:48:41 +0800

    Q1

---
+++ 114上期末上機/Q1/Q1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Q1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label3 = new System.Windows.Forms.Label();
+            this.stoneButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.scissorButton = new System.Windows.Forms.Button();
+            this.paperButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.computerPictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "電腦出";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // stoneButton
+            // 
+            this.stoneButton.Location = new System.Drawing.Point(194, 415);
+            this.stoneButton.Name = "stoneButton";
+            this.stoneButton.Size = new System.Drawing.Size(140, 72);
+            this.stoneButton.TabIndex = 1;
+            this.stoneButton.Text = "石頭";
+            this.stoneButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(563, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 18);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "玩家出";
+            // 
+            // scissorButton
+            // 
+            this.scissorButton.Location = new System.Drawing.Point(657, 415);
+            this.scissorButton.Name = "scissorButton";
+            this.scissorButton.Size = new System.Drawing.Size(140, 72);
+            this.scissorButton.TabIndex = 3;
+            this.scissorButton.Text = "剪刀";
+            this.scissorButton.UseVisualStyleBackColor = true;
+            // 
+            // paperButton
+            // 
+            this.paperButton.Location = new System.Drawing.Point(418, 415);
+            this.paperButton.Name = "paperButton";
+            this.paperButton.Size = new System.Drawing.Size(140, 72);
+            this.paperButton.TabIndex = 4;
+            this.paperButton.Text = "布";
+            this.paperButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(368, 549);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(244, 82);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "結束遊戲";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(508, 394);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // computerPictureBox
+            // 
+            this.computerPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("computerPictureBox.Image")));
+            this.computerPictureBox.Location = new System.Drawing.Point(220, 272);
+            this.computerPictureBox.Name = "computerPictureBox";
+            this.computerPictureBox.Size = new System.Drawing.Size(232, 124);
+            this.computerPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.computerPictureBox.TabIndex = 8;
+            this.computerPictureBox.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(511, 272);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(250, 124);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1240, 1064);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.computerPictureBox);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.paperButton);
+            this.Controls.Add(this.scissorButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.stoneButton);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button stoneButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button scissorButton;
+        private System.Windows.Forms.Button paperButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox computerPictureBox;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ 114上期末上機/Q1/Q1/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Q1
+{
+    public enum Choice
+    {
+        None = -1,              
+        Rock = 0,
+        Paper = 1,
+        Scissor = 2
+    }
+
+    public partial class Form1 : Form
+    {
+        private Random rnd = new Random();
+        private Choice compChoice = Choice.None;
+        private Choice playerChoice = Choice.None;
+        private int playerWins = 0;
+        private int compWins = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 初始化畫面：清空圖片與結果
+            playerPictureBox.Image = null;
+            computerPictureBox.Image = null;
+            resultLabel.Text = "請選擇：石頭、布或剪刀";
+            UpdateScoreLabels();
+        }
+
+        // 產生電腦的隨機選擇
+        private Choice getCompChoice()
+        {
+            int v = rnd.Next(0, 3); // 0,1,2
+            compChoice = (Choice)v;
+            return compChoice;
+        }
+
+        // 主程式：當玩家按按鈕時呼叫
+        private void PlayRound(Choice player)
+        {
+            playerChoice = player;
+            getCompChoice();
+
+            showPlayerImage();
+            showComputerImage();
+
+            showWinner();
+        }
+
+        // 根據玩家選擇顯示圖片
+        private void showPlayerImage()
+        {
+            // 指定圖片（例：玩家出石頭）
+            if (playerPictureBox.Image != null) { playerPictureBox.Image.Dispose(); }
+            playerPictureBox.Image = new Bitmap(Properties.Resources.Rock);
+        }
+
+        // 根據電腦選擇顯示圖片
+        private void showComputerImage()
+        {
+            // 電腦出剪刀
+            if (computerPictureBox.Image != null) { computerPictureBox.Image.Dispose(); }
+            computerPictureBox.Image = new Bitmap(Properties.Resources.Scissor);
+        }
+
+        // 判斷勝負、更新分數、顯示結果
+        private void showWinner()
+        {
+            if (playerChoice == compChoice)
+            {
+                resultLabel.Text = "平手，不計分";
+                return;
+            }
+
+            bool playerWinsRound = false;
+            // 石頭(Rock)贏 剪刀(Scissor)
+            // 剪刀(Scissor)贏 布(Paper)
+            // 布(Paper)贏 石頭(Rock)
+            if (playerChoice == Choice.Rock && compChoice == Choice.Scissor) playerWinsRound = true;
+            else if (playerChoice == Choice.Scissor && compChoice == Choice.Paper) playerWinsRound = true;
+            else if (playerChoice == Choice.Paper && compChoice == Choice.Rock) playerWinsRound = true;
+            else playerWinsRound = false;
+
+            if (playerWinsRound)
+            {
+                playerWins++;
+                resultLabel.Text = "你贏了！";
+            }
+            else
+            {
+                compWins++;
+                resultLabel.Text = "電腦贏了！";
+            }
+
+            UpdateScoreLabels();
+        }
+
+        private void UpdateScoreLabels()
+        {
+            playerLabel.Text = $"玩家：{playerWins}";
+            computerLabel.Text = $"電腦：{compWins}";
+        }
+
+        // 建立代表出拳的影像（動態繪製）
+        private Image CreateChoiceImage(Choice c, bool isPlayer, int width, int height)
+        {
+            Bitmap bmp = new Bitmap(Math.Max(1, width), Math.Max(1, height));
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.Clear(Color.White);
+
+                // 繪製邊框與標題
+                string title = isPlayer ? "玩家" : "電腦";
+                using (Font f = new Font("Segoe UI", 10))
+                using (Brush tb = Brushes.Black)
+                {
+                    g.DrawString(title, f, tb, 6, 6);
+                }
+
+                // 主要繪圖區
+                Rectangle area = new Rectangle(10, 30, bmp.Width - 20, bmp.Height - 40);
+
+                switch (c)
+                {
+                    case Choice.Rock:
+                        // 石頭：灰色圓形
+                        using (Brush b = new SolidBrush(Color.FromArgb(160, 160, 160)))
+                        {
+                            int pad = Math.Min(area.Width, area.Height) / 6;
+                            Rectangle r = new Rectangle(area.X + pad, area.Y + pad, area.Width - pad * 2, area.Height - pad * 2);
+                            g.FillEllipse(b, r);
+                            using (Pen p = new Pen(Color.DimGray, 3)) g.DrawEllipse(p, r);
+                        }
+                        break;
+                    case Choice.Paper:
+                        // 布：白色矩形 + 紋理線
+                        using (Brush b = new SolidBrush(Color.FromArgb(250, 250, 240)))
+                        {
+                            Rectangle r = new Rectangle(area.X + 10, area.Y + 10, area.Width - 20, area.Height - 20);
+                            g.FillRectangle(b, r);
+                            using (Pen p = new Pen(Color.SlateGray, 2)) g.DrawRectangle(p, r);
+                            // 細線表示紙張紋理
+                            using (Pen lp = new Pen(Color.LightGray, 1))
+                            {
+                                for (int y = r.Top + 6; y < r.Bottom; y += 8)
+                                    g.DrawLine(lp, r.Left + 4, y, r.Right - 4, y);
+                            }
+                        }
+                        break;
+                    case Choice.Scissor:
+                        // 剪刀：兩片交叉線 + 把手圓點
+                        using (Pen p = new Pen(Color.SteelBlue, 6))
+                        {
+                            Point center = new Point(area.Left + area.Width / 2, area.Top + area.Height / 2);
+                            p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                            p.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                            g.DrawLine(p, center.X - 40, center.Y - 30, center.X + 40, center.Y + 30);
+                            g.DrawLine(p, center.X - 40, center.Y + 30, center.X + 40, center.Y - 30);
+                        }
+                        using (Brush b = new SolidBrush(Color.SteelBlue))
+                        {
+                            g.FillEllipse(b, area.Right - 30, area.Bottom - 30, 16, 16);
+                            g.FillEllipse(b, area.Left + 10, area.Bottom - 30, 16, 16);
+                        }
+                        break;
+                    default:
+                        // None: 顯示提示
+                        using (Font f = new Font("Segoe UI", 10, FontStyle.Italic))
+                        using (Brush tb = Brushes.Gray)
+                        {
+                            g.DrawString("尚未出拳", f, tb, area.X + 8, area.Y + area.Height / 2 - 8);
+                        }
+                        break;
+                }
+            }
+
+            return bmp;
+        }
+
+        private Image GetResourceImageForChoice(Choice c, bool isPlayer)
+        {
+            string name = c switch
+            {
+                Choice.Rock => isPlayer ? "Rock_Player" : "Rock_Computer",
+                Choice.Paper => isPlayer ? "Paper_Player" : "Paper_Computer",
+                Choice.Scissor => isPlayer ? "Scissor_Player" : "Scissor_Computer",
+                _ => null
+            };
+
+            if (string.IsNullOrEmpty(name)) return null;
+            var obj = Properties.Resources.ResourceManager.GetObject(name);
+            return obj as Image != null ? new Bitmap((Image)obj) : null;
+        }
+
+        // 事件處理：玩家選石頭
+        private void stoneButton_Click(object sender, EventArgs e)
+        {
+            PlayRound(Choice.Rock);
+        }
+
+        // 玩家選布
+        private void paperButton_Click(object sender, EventArgs e)
+        {
+            PlayRound(Choice.Paper);
+        }
+
+        // 玩家選剪刀
+        private void scissorButton_Click(object sender, EventArgs e)
+        {
+            PlayRound(Choice.Scissor);
+        }
+
+        // 結束遊戲並顯示統計
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            string msg = $"遊戲結束\n\n玩家勝場：{playerWins}\n電腦勝場：{compWins}\n\n按確定關閉程式。";
+            MessageBox.Show(msg, "遊戲統計", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.Exit();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
